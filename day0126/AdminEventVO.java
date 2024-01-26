5df0eff6b1f969868bb776e443bd9afa93cdd852
yyyxon <130555434+yyyxon@users.noreply.github.com>
2024-01-26 22:17:42 +0900

    p3_AdminEventVO

---
+++ day0126/AdminEventVO.java
@@ -0,0 +1,12 @@
+package kr.co.sist.admin.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AdminEventVO {
+	private String title, img, img2, id, eventcode, context, start_date, input_date, end_date;
+}
