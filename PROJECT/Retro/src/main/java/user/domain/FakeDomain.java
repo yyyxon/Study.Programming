441f3c68d9447ace2ca7eec36da37b8e4336ec07
Seungyeon Yang <y_eoniii@naver.com>
2024-05-23 20:33:05 +0900

    Create User Fake Domain

---
+++ PROJECT/Retro/src/main/java/user/domain/FakeDomain.java
@@ -0,0 +1,12 @@
+package kr.co.sist.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FakeDomain {
+	private int cnt;
+	private String fraudKeyword;
+
+}
