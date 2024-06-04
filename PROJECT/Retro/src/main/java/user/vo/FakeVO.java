682fb49608b1f65c0aa37927c930c4b9a75e29cb
Seungyeon Yang <y_eoniii@naver.com>
2024-06-04 23:13:59 +0900

    Create Fake VO

---
+++ PROJECT/Retro/src/main/java/user/vo/FakeVO.java
@@ -0,0 +1,12 @@
+package kr.co.sist.user.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class FakeVO {
+	private String fraudField, fraudKeyword;
+}
