e26e7a345daef63ece952fde4ef10fe6fc0ebf5e
Seungyeon Yang <y_eoniii@naver.com>
2024-08-25 22:39:45 +0900

    JAVA

---
+++ JAVA/JFC/EmpTab2.java
@@ -0,0 +1,13 @@
+package projectEx;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class EmpTab2 extends JPanel {
+	private JButton jbtn;
+	public EmpTab2() {
+		
+		jbtn=new JButton("탭2에서 제공할 내용");
+		add(jbtn);
+	}
+}
